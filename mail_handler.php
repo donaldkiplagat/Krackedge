cba1b8c7993c67b664e69d082f7a07568e5f4c74
donaldkiplagat <donald.k.kiplagat@gmail.com>
2019-11-06 18:56:10 +0300

    Mailing forms added'

--- mail_handler.php
+++ mail_handler.php
@@ -8,12 +8,13 @@ if(isset($_POST['submit'])){
     $message = $client_name . " wrote the following:" . "\n\n" . $_POST['message'];
     $message2 = "Here is a copy of your message " . $client_name . "\n\n" . $_POST['message'];
 
+
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // echo "Mail Sent. Thank you " . $client_name . ", we will contact you shortly.";
+    header('Location: thankyou.html');
     // You cannot use header and echo together. It's one or the other.
     }
 ?>
