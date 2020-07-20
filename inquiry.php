26a64838202415e2161c562815cd05a5497ee994
donaldkiplagat <donald.k.kiplagat@gmail.com>
2020-07-20 20:11:46 +0300

    Sitemap added

--- inquiry.php
+++ inquiry.php
@@ -1,9 +1,16 @@
 <?php
 
 if(isset($_POST['submit'])){
+  // robot detection
+  $honeypot = $_POST["email"];
+
+  if(!empty($honeypot)) {
+    echo "BAD ROBOT!";
+    exit;
+  }
 
   $client_name = $_POST['client_name'];
-  $email = $_POST['email'];
+  $baruameme = $_POST['baruameme'];
   $message = $_POST['message'];
   $subject = $_POST['subject'];
 
@@ -15,7 +22,7 @@ if(isset($_POST['submit'])){
 
   //Create a new PHPMailer instance
   $mail = new PHPMailer;
-//  $mail->SMTPDebug = 4;                               // Enable verbose debug output
+  //$mail->SMTPDebug = 4;                               // Enable verbose debug output
 
 
   //Tell PHPMailer to use SMTP - requires a local mail server
@@ -28,18 +35,27 @@ if(isset($_POST['submit'])){
   $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
   $mail->Port = 587;                                    // TCP port to connect to
 
+
+
+  $mail->SMTPOptions = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+  );
   //Use a fixed address in your own domain as the from address
   //**DO NOT** use the submitter's address here as it will be forgery
   //and will cause your messages to fail SPF checks
-  $mail->setFrom('info@krackedgecreative.com', 'Krackedge Creative');
+  $mail->setFrom(EMAIL, 'Krackedge Creative');
   //Send the message to yourself, or whoever should receive contact for submissions
-  $mail->addAddress('info@krackedgecreative.com', 'Krackedge Creative');
-  $mail->addBCC('donald.k.kiplagat@gmail.com');
+  $mail->addAddress(EMAIL, 'Krackedge Creative');
+  $mail->addBCC(EMAIL2);
 
   //Put the submitter's address in a reply-to header
   //This will fail if the address provided is invalid,
   //in which case we should ignore the whole request
-  if ($mail->addReplyTo($email,$email)) {
+  if ($mail->addReplyTo($baruameme,$baruameme)) {
       $mail->Subject = '[Krackedge Creative Contact Form] - '.$subject;
       //Keep it simple - don't use HTML
       $mail->isHTML(false);
@@ -47,7 +63,7 @@ if(isset($_POST['submit'])){
 
       $mail->Body = <<<EOT
 Name: {$client_name}
-Email: {$email}
+Email: {$baruameme}
 
 
 {$client_name} wrote the following:
@@ -59,7 +75,7 @@ EOT;
       //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 
-//      echo 'Mailer Error: ' . $mail->ErrorInfo;
+     // echo 'Mailer Error: ' . $mail->ErrorInfo;
 
       //Send the message, check for errors
       if (!$mail->send()) {
@@ -77,14 +93,14 @@ Thank you for contacting our team. Your inquiry has been received and a response
 The details of your inquiry are as shown below:
 
 Name: {$client_name}
-Email: {$email}
+Email: {$baruameme}
 
 {$message}
 
 Kind Regards,
 Krackedge Creative Team
 EOT;
-        $mail->AddAddress($email);
+        $mail->AddAddress($baruameme);
         $mail->Send();
 
         header("Location:thankyou.html");
